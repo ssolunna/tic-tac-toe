89ddc3831967b625673f7060479082c0c574c3c3
ssolunna <ssolunna29@gmail.com>
2023-01-31 14:55:55 -0500

    Display empty board

---
+++ tic_tac_toe.rb
@@ -0,0 +1,8 @@
+# Game: Tic Tac Toe
+
+board = { C1R1: "_", C2R1: "_", C3R1: "_",
+          C1R2: "_", C2R2: "_", C3R2: "_",
+          C1R3: "_", C2R3: "_", C3R3: "_" } 
+puts "_#{board[:C1R1]}_|_#{board[:C2R1]}_|_#{board[:C3R1]}_"
+puts "_#{board[:C1R2]}_|_#{board[:C2R2]}_|_#{board[:C3R2]}_"
+puts "_#{board[:C1R3]}_|_#{board[:C2R3]}_|_#{board[:C3R3]}_"
