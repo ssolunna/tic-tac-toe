30801c383a6f10e6a62dc5829049db3f9fbf8116
ssolunna <ssolunna29@gmail.com>
2023-01-31 15:48:13 -0500

    Add method to display marks on the board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,8 +1,26 @@
 # Game: Tic Tac Toe
 
-board = { C1R1: "_", C2R1: "_", C3R1: "_",
-          C1R2: "_", C2R2: "_", C3R2: "_",
-          C1R3: "_", C2R3: "_", C3R3: "_" } 
-puts "_#{board[:C1R1]}_|_#{board[:C2R1]}_|_#{board[:C3R1]}_"
-puts "_#{board[:C1R2]}_|_#{board[:C2R2]}_|_#{board[:C3R2]}_"
-puts "_#{board[:C1R3]}_|_#{board[:C2R3]}_|_#{board[:C3R3]}_"
+class Board
+  @@board = { C1R1: "_", C2R1: "_", C3R1: "_",
+              C1R2: "_", C2R2: "_", C3R2: "_",
+              C1R3: "_", C2R3: "_", C3R3: "_" }
+
+  def Board.display
+    puts "_#{@@board[:C1R1]}_|_#{@@board[:C2R1]}_|_#{@@board[:C3R1]}_"
+    puts "_#{@@board[:C1R2]}_|_#{@@board[:C2R2]}_|_#{@@board[:C3R2]}_"
+    puts "_#{@@board[:C1R3]}_|_#{@@board[:C2R3]}_|_#{@@board[:C3R3]}_"
+  end
+end
+
+class Player < Board
+  attr_reader :name, :mark
+  
+  def initialize(name, mark)
+    @name = name
+    @mark = mark
+  end
+
+  def mark_on_board(position)
+    @@board[position] = @mark
+  end
+end
