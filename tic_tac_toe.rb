b782d595efcdeaea8994cad6b3cd107055c69dce
ssolunna <ssolunna29@gmail.com>
2023-01-31 18:01:43 -0500

    Add Player attribute to save the positions marked

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,11 +1,11 @@
 # Game: Tic Tac Toe
 
 class Board
-  @@board = { C1R1: "_", C2R1: "_", C3R1: "_",
-              C1R2: "_", C2R2: "_", C3R2: "_",
-              C1R3: "_", C2R3: "_", C3R3: "_" }
+  @@board = { C1R1: '_', C2R1: '_', C3R1: '_',
+              C1R2: '_', C2R2: '_', C3R2: '_',
+              C1R3: '_', C2R3: '_', C3R3: '_' }
 
-  def Board.display
+  def self.display
     puts "     C1  C2  C3\n\n"
     puts "R1   _#{@@board[:C1R1]}_|_#{@@board[:C2R1]}_|_#{@@board[:C3R1]}_"
     puts "R2   _#{@@board[:C1R2]}_|_#{@@board[:C2R2]}_|_#{@@board[:C3R2]}_"
@@ -14,15 +14,16 @@ class Board
 end
 
 class Player < Board
-  attr_reader :name, :mark
-  
+  attr_reader :name, :mark, :marked
+
   def initialize(name, mark)
     @name = name
     @mark = mark
+    @marked = []
   end
 
   def mark_on_board(position)
-    @@board[position] = @mark
+    @@board[position.to_sym] = @mark
+    @marked.push(position)
   end
 end
-
