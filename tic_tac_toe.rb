0e3490994cc6c2c4c914a63964734a825aec26a7
ssolunna <ssolunna29@gmail.com>
2023-01-31 16:04:08 -0500

    Add column and row labels

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -6,9 +6,10 @@ class Board
               C1R3: "_", C2R3: "_", C3R3: "_" }
 
   def Board.display
-    puts "_#{@@board[:C1R1]}_|_#{@@board[:C2R1]}_|_#{@@board[:C3R1]}_"
-    puts "_#{@@board[:C1R2]}_|_#{@@board[:C2R2]}_|_#{@@board[:C3R2]}_"
-    puts "_#{@@board[:C1R3]}_|_#{@@board[:C2R3]}_|_#{@@board[:C3R3]}_"
+    puts "     C1  C2  C3\n\n"
+    puts "R1   _#{@@board[:C1R1]}_|_#{@@board[:C2R1]}_|_#{@@board[:C3R1]}_"
+    puts "R2   _#{@@board[:C1R2]}_|_#{@@board[:C2R2]}_|_#{@@board[:C3R2]}_"
+    puts "R3   _#{@@board[:C1R3]}_|_#{@@board[:C2R3]}_|_#{@@board[:C3R3]}_"
   end
 end
 
@@ -24,3 +25,4 @@ class Player < Board
     @@board[position] = @mark
   end
 end
+
