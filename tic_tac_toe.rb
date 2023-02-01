82a1cdcfe6ba239337d7bb3361c4b7b99a65d382
ssolunna <ssolunna29@gmail.com>
2023-02-01 17:14:19 -0500

    Check for the Game winner

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -16,14 +16,45 @@ end
 class Player < Board
   attr_reader :name, :mark, :marked
 
+  attr_accessor :winner
+
+  @@players = []
+
   def initialize(name, mark)
     @name = name
     @mark = mark
     @marked = []
+    @winner = false
+    @@players << self
   end
 
   def mark_on_board(position)
     @@board[position.to_sym] = @mark
-    @marked.push(position)
+    @marked << position
+  end
+end
+
+class Game < Player
+  @@ways_to_win = [
+    %w[C1R1 C2R1 C3R1],
+    %w[C1R2 C2R2 C3R2],
+    %w[C1R3 C2R3 C3R3],
+    %w[C1R1 C1R2 C1R3],
+    %w[C2R1 C2R2 C2R3],
+    %w[C3R1 C3R2 C3R3],
+    %w[C1R1 C2R2 C3R3],
+    %w[C3R1 C2R2 C1R3]
+  ]
+
+  def self.winner?
+    @@players.each do |player|
+      @@ways_to_win.each do |way|
+        if way.all? { |position| player.marked.include?(position) }
+          player.winner = true
+          return true
+        end
+      end
+    end
+    false
   end
 end
