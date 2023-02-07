51819ce2dfa69c011594ea288e6083cdb862c93e
ssolunna <ssolunna29@gmail.com>
2023-02-07 16:48:03 -0500

    Check that players uses the correct syntax to mark

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -6,11 +6,13 @@ class Board
               C1R3: '_', C2R3: '_', C3R3: '_' }
 
   def self.display
+    puts
     puts '     C1  C2  C3'
     puts
     puts "R1   _#{@@board[:C1R1]}_|_#{@@board[:C2R1]}_|_#{@@board[:C3R1]}_"
     puts "R2   _#{@@board[:C1R2]}_|_#{@@board[:C2R2]}_|_#{@@board[:C3R2]}_"
     puts "R3   _#{@@board[:C1R3]}_|_#{@@board[:C2R3]}_|_#{@@board[:C3R3]}_"
+    puts
   end
 end
 
@@ -31,8 +33,31 @@ class Player < Board
 
   def mark_on_board
     position = gets.chomp.upcase
-    @@board[position.to_sym] = @mark
-    @marked << position
+    
+    if /^C[123]R[123]$/.match?(position)
+      if @@board[position.to_sym] == '_'
+        @@board[position.to_sym] = @mark
+        @marked << position
+
+      elsif self.marked.include?(position)
+        puts 'You have already marked this space.'
+        Board.display
+        print "#{@name} ('#{@mark}'), choose an empty one: "
+        self.mark_on_board
+
+      else
+        puts 'This space has already been marked.'
+        Board.display
+        print "#{@name} ('#{@mark}'), choose another one: "
+        self.mark_on_board
+      end
+
+    else
+      puts 'Wrong syntax.'
+      Game.help
+      print "#{@name} ('#{@mark}'), try again: "
+      self.mark_on_board
+    end
   end
 
   def self.all
@@ -53,21 +78,24 @@ class Game < Player
   ]
 
   def self.info
+    puts
     puts 'Game: Tic-tac-toe'
     puts
     puts 'Tic-tac-toe is played on a three-by-three grid by two players,'
     puts "who alternately place the marks 'X' and 'O' in one of the nine"
     puts 'spaces in the grid.'
+    puts
+    puts "Player 1's mark is 'X' and Player 2's mark is 'O'."
     puts 'The player who succeeds in placing three of their marks in a'
     puts 'horizontal, vertical, or diagonal row is the winner.'
+    puts
   end
 
   def self.help
-    puts "Player 1's mark is 'X' and Player 2's mark is 'O'."
+    puts
     puts 'The syntax to place your mark on the board is "C[123]R[123]" where'
     puts '"C" stands for Column and "R" for Row; [123] means to type 1, 2 or 3.'
     puts '(e.g., "C1R2" places your mark in the first column and second row)'
-    puts
     Board.display
   end
 
@@ -85,9 +113,7 @@ class Game < Player
 end
 
 Game.info
-puts
 Game.help
-puts
 
 Player.new('Player 1', 'X')
 Player.new('Player 2', 'O')
@@ -96,9 +122,7 @@ until Game.winner?
   Player.all.each do |player|
     print "#{player.name} ('#{player.mark}') turn: "
     player.mark_on_board
-    puts
     Board.display
-    puts
     break if Game.winner?
   end
 end
