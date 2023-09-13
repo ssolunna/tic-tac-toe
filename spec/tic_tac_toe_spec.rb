c15ed25037c5af91e5ca52e3aa090b8ba8ea41cd
ssolunna <ssolunna29@gmail.com>
2023-09-12 22:19:14 -0500

    Add another test

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -15,9 +15,15 @@ describe Player do
   describe '#mark_on_board' do
     context 'when user inputs a valid position' do
       let(:valid_position) { 'C1R1' }
+
       player_valid_input = described_class.new('Player 1', 'X')
 
-      context 'when the position on the board is empty' do
+      before do
+        allow(Board).to receive(:display)
+        allow(player_valid_input).to receive(:print)
+      end
+
+      context "when the board's position is empty" do
         before do
           allow(player_valid_input).to receive(:gets).and_return(valid_position)
         end
@@ -38,6 +44,38 @@ describe Player do
         it 'does not display any puts statements' do
           expect(player_valid_input).not_to receive(:puts)
         end
+      end
+
+      context "when the board's position is not empty" do
+        before do
+          another_valid_position = 'C2R2'
+          allow(player_valid_input).to receive(:gets).and_return(valid_position, another_valid_position)
+        end
+
+       it 'calls for another input' do
+          expect(player_valid_input).to receive(:mark_on_board).once
+          player_valid_input.mark_on_board
+        end
+      end
+    end
+    
+    context 'when user inputs an invalid position' do
+      player_invalid_input = described_class.new('Player 2', 'O')
+
+      before do
+        allow(Game).to receive(:help)
+        allow(player_invalid_input).to receive(:print)
+
+        invalid_position = 'C4R4'
+        valid_position = 'C3R3'
+
+        allow(player_invalid_input).to receive(:gets).and_return(invalid_position, valid_position)
+      end
+
+      it "displays error message" do
+        error_message = 'Wrong syntax.'
+        expect(player_invalid_input).to receive(:puts).with(error_message).once
+        player_invalid_input.mark_on_board
       end
     end
   end
